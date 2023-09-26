2a9dfce6cc2cb696d7212d9049679088d8cad016
CarlosSamaniegoLeo <carlossama10@gmail.com>
2023-09-26 09:17:45 +0200

    Clase Empleado

---
+++ src/main/java/org/example/Empleado.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Empleado {
+    int dni;
+
+    public Empleado(int dni) {
+        this.dni = dni;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+}
